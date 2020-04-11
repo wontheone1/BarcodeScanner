38a1b7552102f24c84d318442298f1ace24a78a1
Won Seob Seo <won.seob.seo@zalando.fi>
2020-04-11 15:24:18 +0300

    Listen to keyboard input

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-/**
- * Do not modify this file - it is a proxy to your `App.re` file
- * located in the `src/` folder.
- */
-export { app as default } from "./src/App.bs.js";
+import React from 'react';
+import KeyEvent from 'react-native-keyevent';
+
+import {View, Text} from 'react-native';
+
+const KeyEventListener = () => {
+  React.useEffect(() => {
+    KeyEvent.onKeyDownListener(keyEvent => {
+      console.log('onKeyDownListener');
+      console.log(`onKeyDown keyCode: ${keyEvent.keyCode}`);
+      console.log(`Action: ${keyEvent.action}`);
+      console.log(`Key: ${keyEvent.pressedKey}`);
+    });
+  });
+  return (
+    <View>
+      <Text>Registering key listener</Text>
+    </View>
+  );
+};
+
+export default KeyEventListener;
